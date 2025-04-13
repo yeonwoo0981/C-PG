a7760c0173e4b40ebc576dd02f068f485a6e09a2
ByulDaram <0981syw@naver.com>
2025-04-13 15:16:36 +0900

    scenechanger
    씬변화구성완료

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -1,16 +1,67 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Slider progressBar;
+    public Text progressText; 
+
     void Start()
     {
         
+        StartCoroutine(LoadGameScene());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator LoadGameScene()
     {
         
+        progressBar.value = 0f;
+
+       
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("GameScene1");
+        asyncLoad.allowSceneActivation = false; 
+
+        while (asyncLoad.progress < 0.9f) 
+        {
+           
+            float progress = Mathf.Clamp01(asyncLoad.progress / 0.9f);
+            progressBar.value = progress;
+
+            
+            if (progressText != null)
+            {
+                progressText.text = $"로딩 중... {Mathf.Floor(progress * 100)}%";
+            }
+
+            yield return null;
+        }
+
+        
+        float fakeProgress = progressBar.value;
+        while (fakeProgress < 1.0f)
+        {
+            fakeProgress += 0.01f;
+            progressBar.value = fakeProgress;
+
+            if (progressText != null)
+            {
+                progressText.text = $"로딩 중... {Mathf.Floor(fakeProgress * 100)}%";
+            }
+
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        
+        progressBar.value = 1.0f;
+        if (progressText != null)
+        {
+            progressText.text = "로딩 완료!";
+        }
+
+        
+        yield return new WaitForSeconds(0.5f);
+        asyncLoad.allowSceneActivation = true;
     }
 }
--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartButton : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Slider volumeSlider;
+
+
     void Start()
     {
         
+        if (volumeSlider != null)
+        {
+            volumeSlider.value = PlayerPrefs.GetFloat("GameVolume", 0.5f);
+            volumeSlider.onValueChanged.AddListener(SetVolume);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetVolume(float volume)
     {
         
+        PlayerPrefs.SetFloat("GameVolume", volume);
+        AudioListener.volume = volume;
     }
+
+    public void StartGame()
+    {
+        
+        SceneManager.LoadScene("LoadingScene");
+    }
+
 }
