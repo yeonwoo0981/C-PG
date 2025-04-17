679205ce95314e5317e3da2bc768f8927e5e104a
ForRest1217 <Jihan2519@naver.com>
2025-04-17 18:59:31 +0900

    enemy maybe wansung
    haha

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -3,24 +3,66 @@ using UnityEngine;
 public class EnemyMove : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rigid;
-    private float speed = 2;
+    private float speed = 2f;
     private Vector2 vec;
     private GameObject player;
+
+    private Animator ani;
+
+    private float attackrange = 2f;
+    private float range;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
     }
 
     public void Update()
     {
+        Debug.Log(range);
         player = GameObject.FindGameObjectWithTag("Player");
+        range = Vector2.Distance(transform.position, player.transform.position);
         vec = player.transform.position - transform.position;
     }
     public void FixedUpdate()
     {
-        rigid.linearVelocity = vec.normalized * speed;
+        locate();
+        AttackLenght();
+        AnimationRun();
     }
 
+    void locate()
+    {
+        if (vec.x > 0)
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        else if (vec.x < 0)
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+    }
 
+    void AttackLenght()
+    {
+        if (range <= attackrange)
+        {
+            ani.SetTrigger("attack");
+            speed = 0f;
+        }
+        else
+        {
+            speed = 2f;
+            rigid.linearVelocity = vec.normalized * speed;
+        }
+    }
+
+    void AnimationRun()
+    {
+        if (vec.x > 0 || vec.x < 0)
+        {
+            ani.SetBool("run", true);
+        }
+        else
+        {
+            ani.SetBool("run", true);
+        }
+    }
 }
