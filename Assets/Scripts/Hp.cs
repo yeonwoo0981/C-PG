6aeb80b21e06650bb414200cd6a5ccb6605bbc06
ByulDaram <0981syw@naver.com>
2025-04-23 20:02:17 +0900

    hptext
    hp바에 현재체력/최대체력 추가 완.

--- Assets/Scripts/Hp.cs
+++ Assets/Scripts/Hp.cs
@@ -1,45 +1,63 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro; 
 
 public class Hp : MonoBehaviour
 {
     [SerializeField] private float curHealth = 100f;
     [SerializeField] private float maxHealth = 100f;
     [SerializeField] private GameObject hpScripts;
+
     
+    [SerializeField] private TextMeshProUGUI hpText; 
+
+    public Slider HpBarSlider;
+
     private void Start()
     {
         Debug.Log($"Ω√¿€ Ω√ HP: {curHealth}/{maxHealth}");
+
+       
+        if (hpText == null)
+        {
+            Transform textTransform = transform.Find("hpText");
+            if (textTransform != null)
+            {
+                hpText = textTransform.GetComponent<TextMeshProUGUI>();
+                
+            }
+        }
+
         CheckHp();
     }
-    private void SetHp(float amount) 
+
+    private void SetHp(float amount)
     {
         maxHealth = amount;
         curHealth = maxHealth;
+        CheckHp(); 
     }
-    public Slider HpBarSlider;
-     
-    public void CheckHp() 
+
+    public void CheckHp()
     {
         if (HpBarSlider != null)
             HpBarSlider.value = curHealth / maxHealth;
 
-        
        
+        if (hpText != null)
+            hpText.text = $"{curHealth}/{maxHealth}";
     }
 
-    private void Update()
-    {
-        
-       
-    }
+    
 
-    public void Damage(float damage) 
+    public void Damage(float damage)
     {
-        if (maxHealth == 0 || curHealth <= 0) 
+        if (maxHealth == 0 || curHealth <= 0)
             return;
+
         curHealth -= damage;
-         
+        CheckHp(); 
+
         if (curHealth <= 0)
         {
             Destroy(gameObject);
