6a187eeb6e5459c2615318c3a6abdb906240cedb
ByulDaram <0981syw@naver.com>
2025-04-24 19:10:59 +0900

    stamina
    미나미나스태미나

--- Assets/Scripts/StmScript.cs
+++ Assets/Scripts/StmScript.cs
@@ -1,11 +1,15 @@
+using TMPro;
 using TMPro.EditorUtilities;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StmScript : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI stmText;
     public Dash qustn;
     private float _stm;
     private float _maxstm;
+    public Slider stmBarSlider;
     void Awake()
     {
         qustn = GameObject.Find("Player").GetComponent<Dash>();
@@ -18,8 +22,35 @@ public class StmScript : MonoBehaviour
     private void Start()
     {
         _stm = _maxstm;
+        if (stmText == null)
+        {
+            Transform textTransform = transform.Find("stmText");
+            if (textTransform != null)
+            {
+                stmText = textTransform.GetComponent<TextMeshProUGUI>();
+
+            }
+        }
+
+        CheckHp();
     }
 
+    private void SetHp(float amount)
+    {
+        _maxstm = amount;
+        _stm = _maxstm;
+        CheckHp();
+    }
+
+    public void CheckHp()
+    {
+        if (stmBarSlider != null)
+            stmBarSlider.value = _stm / _maxstm;
+
+
+        if (stmText != null)
+            stmText.text = $"{_stm:F1}/{_maxstm}";
+    }
     public void stm_gaugeMin()
     {
         if (_stm <= 20)
@@ -30,13 +61,15 @@ public class StmScript : MonoBehaviour
         {
             _stm -= 20f;
             _stm = Mathf.Clamp(_stm, 0, _maxstm);
-            Debug.Log(_stm);
+            Debug.Log($"Stamina: {_stm}");
         }
+        CheckHp();
     }
     public void stm_gaugePlus()
     {
         _stm += 4 * Time.deltaTime;
         _stm = Mathf.Clamp(_stm, 0, _maxstm);
-        Debug.Log(_stm);
+        Debug.Log($"Stamina: {_stm}");
+        CheckHp();
     }
 }
