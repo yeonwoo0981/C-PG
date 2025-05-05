eba442821308761570ae8b724019e51dc483adcf
ByulDaram <0981syw@naver.com>
2025-05-06 02:11:53 +0900

    lvuptx
    1

--- Assets/Scripts/levelUp.cs
+++ Assets/Scripts/levelUp.cs
@@ -1,11 +1,21 @@
 using UnityEngine;
+using TMPro;
+using System.Collections;
 
 public class levelUp : MonoBehaviour
 {
     public int level = 1;
     public int p_Ex = 0;
     public Hp playerHp;
+    [SerializeField] private TextMeshProUGUI _lvUpText;
 
+    private void Start()
+    {
+        if (_lvUpText != null)
+        {
+            _lvUpText.gameObject.SetActive(false);
+        }
+    }
     public void ExUp()
     {
         p_Ex += 55;
@@ -24,6 +34,21 @@ public class levelUp : MonoBehaviour
         Debug.Log($"플레이어 레벨: {level}");
         HpPlus();
     }
+
+    private IEnumerator ShowLevelUpText()
+    {
+        if (_lvUpText != null)
+        {
+
+            _lvUpText.gameObject.SetActive(true);
+            _lvUpText.text = $"LEVEL UP!";
+
+            yield return new WaitForSeconds(3f);
+
+            _lvUpText.gameObject.SetActive(false);
+        }
+    }
+
 
     public void HpPlus()
     {
