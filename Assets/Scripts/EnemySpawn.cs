5d02b433c45ee85f2f533c9fe5395b8483ee9c95
ForRest1217 <Jihan2519@naver.com>
2025-05-06 22:47:11 +0900

    dadw
    adwadw

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject prefabs;
+    private Vector2 spawnx = new Vector2(5f, 23f);
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        for (int i = 0; i < 3; i++)
+        {
+            float randomx = Random.Range(spawnx.x, spawnx.y);
+            Vector2 spawn = new Vector2(randomx, -2.682f);
+            GameObject instant = Instantiate(prefabs, spawn, Quaternion.identity); //오브젝트,스폰위치,회전 없음
+        }
     }
 }
