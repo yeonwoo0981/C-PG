2aa41160d162a8c92306daa4198c0d8c55bbe7d4
ByulDaram <0981syw@naver.com>
2025-05-07 17:35:07 +0900

    2
    2

--- Assets/Scripts/EnemyHP.cs
+++ Assets/Scripts/EnemyHP.cs
@@ -3,18 +3,14 @@ using UnityEngine;
 public class EnemyHP : MonoBehaviour
 {
     public int e_HP = 20;
-    private levelUp p_levelUp;
+    
     private bool E_isDead = false;
 
     private void Start()
     {
         E_isDead = false;
 
-        GameObject levelUpObj = GameObject.FindGameObjectWithTag("Player");
-        if (levelUpObj != null)
-        {
-            p_levelUp = levelUpObj.GetComponent<levelUp>();
-        }
+       
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -41,10 +37,7 @@ public class EnemyHP : MonoBehaviour
     {
         Debug.Log("Enemy Dead");
 
-        if (p_levelUp != null)
-        {
-            p_levelUp.ExUp();
-        }
+       
 
         Destroy(gameObject);
     }
