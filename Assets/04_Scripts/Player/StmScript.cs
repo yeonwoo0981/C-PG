38d9b779ce100665ecd6c7406b27899d018c8f7c
ByulDaram <0981syw@naver.com>
2025-05-17 17:54:16 +0900

    323
    232

--- Assets/04_Scripts/Player/StmScript.cs
+++ Assets/04_Scripts/Player/StmScript.cs
@@ -1,5 +1,4 @@
 using TMPro;
-using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.UI;
 
--- Assets/04_Scripts/SwordDamage.cs
+++ Assets/04_Scripts/SwordDamage.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class SwordDamage : MonoBehaviour
 {
