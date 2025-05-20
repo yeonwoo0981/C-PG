01e6bae7dc7d55839deec6a8bf548df22b966756
ByulDaram <0981syw@naver.com>
2025-05-20 17:30:18 +0900

    1233
    333133111

---
+++ Java/Program/src/App3.java
@@ -0,0 +1,12 @@
+public class App3 {
+    public static void main(String[] args) throws Exception {
+    String weeks[] = {"월요일", "화요일", "수요일", "목요일", "금요일", "토요일", "일요일"};
+    int cnt = 1;
+    for (int i = 1; i < 7; i++){
+        if (i%3 == 0)
+        break;
+        cnt++;
+    }
+    System.out.print("오늘은 " + weeks[cnt]);
+    }
+}
