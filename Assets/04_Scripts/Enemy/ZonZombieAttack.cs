83f5f69813b75e3867f44bcf6608e56ae8ee75ed
ForRest1217 <Jihan2519@naver.com>
2025-05-20 21:26:15 +0900

    d
    d

--- Assets/04_Scripts/Enemy/ZonZombieAttack.cs
+++ Assets/04_Scripts/Enemy/ZonZombieAttack.cs
@@ -2,5 +2,28 @@ using UnityEngine;
 
 public class ZonZombieAttack : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D rigid;
+    private float speed = 2f;
+    private Vector2 vec;
+    private GameObject player;
+    private Transform zombie;
 
+    private void OnEnable()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        transform.position = player.transform.position;
+    }
+
+    private void Update()
+    {
+        locate();
+    }
+
+    void locate()
+    {
+        if (vec.x > 0)
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        else if (vec.x < 0)
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+    }
 }
